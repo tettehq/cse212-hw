d4e97ebb03224dd800e6650b9a72e77672eb5da3
tettehq <ronyquash@gmail.com>
2025-01-15 22:12:43 +0000

    stacks & queues study

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -10,7 +10,7 @@
         queue.Enqueue(100);
         var value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found:
+        // Defect(s) Found: It attempted to remove items using the wrong index 1 instead of 0
 
         Console.WriteLine("------------");
 
@@ -28,7 +28,7 @@
         Console.WriteLine(value);
         value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found: 
+        // Defect(s) Found: It should remove items from the start, not from the end.
 
         Console.WriteLine("------------");
 
@@ -54,7 +54,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value); // Defect 1: Add to the end, not to the start or index 0
     }
 
     /// <summary>
@@ -66,8 +66,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0]; // Defect 1: Changed the index from 1 to 0, which is the actual start.
+        _queue.RemoveAt(0);
         return value;
     }
 }
